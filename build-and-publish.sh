a831b9bf18edc49b50e7129211a72f7c8fbb6b24
crdavip <crdavip@gmail.com>
2025-03-16 02:37:05 -0500

    Repo Reset

--- build-and-publish.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-docker build -t crbeunik.azurecr.io/beunik/blog-api:remoteDebug .
-docker push crbeunik.azurecr.io/beunik/blog-api:remoteDebug
-helm upgrade -i blog-api ./helm -f ./helm/values.yaml --set image=crbeunik.azurecr.io/beunik/blog-api:remoteDebug
