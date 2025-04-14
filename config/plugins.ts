340a8d63f3e325766e62b42e765557872632b2fd
crdavip <crdavip@gmail.com>
2025-04-14 18:35:16 -0500

    Bunny implement

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default () => ({
+  upload: {
+    config: {
+      provider: "@nexide/strapi-provider-bunny",
+      providerOptions: {
+        api_key: "013214a7-b822-4232-ae00bcb00570-ef4e-47cb",
+        storage_zone: "beunik-blog-images",
+        pull_zone: "beunik-blog-pull-zone",
+        hostname: "beunik-blog-pull-zone.b-cdn.net",
+      },
+    },
+  },
+});
